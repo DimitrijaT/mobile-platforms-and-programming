28f59ab53171290088fd06c8d04543ff7f8bc182
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-11-12 17:03:11 +0100

    Implicit Intents (Opening Maps, Sending Mail, Using Calendar, Opening a Web Site)

--- app/src/main/java/mk/ukim/finki/findbyagegroup/MainActivity.kt
+++ app/src/main/java/mk/ukim/finki/findbyagegroup/MainActivity.kt
@@ -1,18 +1,19 @@
 package mk.ukim.finki.findbyagegroup
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import mk.ukim.finki.findbyagegroup.R.id.txtAgeGroup
 import mk.ukim.finki.findbyagegroup.extensions.toInt
 import mk.ukim.finki.findbyagegroup.viewmodels.AgeViewModel
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +21,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editTextAge: EditText
     private lateinit var btnSubmit: Button
     private lateinit var btnGoToExplicitActivity: Button
+    private lateinit var btnGoToImplicitActivityOpenMaps: Button
+    private lateinit var btnGoToImplicitActivitySendMail: Button
+    private lateinit var btnGoToImplicitActivityCalendar: Button
+    private lateinit var btnGoToImplicitActivityWebSite: Button
 
     private lateinit var ageViewModel: AgeViewModel
 
@@ -31,6 +36,10 @@ class MainActivity : AppCompatActivity() {
         editTextAge = findViewById(R.id.editTextAge)
         btnSubmit = findViewById(R.id.btnSubmit)
         btnGoToExplicitActivity = findViewById<Button>(R.id.btnGoToExplicitActivity)
+        btnGoToImplicitActivityOpenMaps = findViewById<Button>(R.id.btnGoToImplicitActivityOpenMaps)
+        btnGoToImplicitActivitySendMail = findViewById<Button>(R.id.btnGoToImplicitActivitySendMail)
+        btnGoToImplicitActivityCalendar = findViewById<Button>(R.id.btnGoToImplicitActivityCalendar)
+        btnGoToImplicitActivityWebSite = findViewById<Button>(R.id.btnGoToImplicitActivityWebSite)
 
 
         // It's instantiated only once, but everytime the activity is created it uses the same reference.
@@ -44,11 +53,70 @@ class MainActivity : AppCompatActivity() {
             ageViewModel.setAgeValue(editTextAge.text.toInt())
         }
 
+        // EXPLICIT INTENT
         btnGoToExplicitActivity.setOnClickListener {
             Intent(this, ExplicitActivity::class.java).let { i ->
                 i.putExtra("ageGroup", editTextAge.text.toString())
                 startActivity(i)
             }
+        }
+
+        // IMPLICIT INTENT - Opening Maps
+        btnGoToImplicitActivityOpenMaps.setOnClickListener {
+//          Android knows that we want to open google maps.
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://maps.google.com/?q=Rugjer%20Boskovikj%20Skopje")
+            ).let { i ->
+                startActivity(i)
+            }
+        }
+
+        // IMPLICIT INTENT - Sending Mail
+        btnGoToImplicitActivitySendMail.setOnClickListener {
+            Intent(Intent.ACTION_SEND).let { emailIntent ->
+                emailIntent.type = "text/plain"
+                emailIntent.putExtra(
+                    Intent.EXTRA_EMAIL,
+                    arrayOf("dimitrijatimeskidimitrija@gmail.com")
+                )
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Email Subject")
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Email Body")
+                startActivity(emailIntent)
+            }
+        }
+
+        // IMPLICIT INTENT - Using Calendar
+        btnGoToImplicitActivityCalendar.setOnClickListener {
+            Intent(
+                Intent.ACTION_INSERT,
+                CalendarContract.Events.CONTENT_URI
+            ).let { calendarIntent ->
+                val beginTime: Calendar = Calendar.getInstance()
+                beginTime.set(2012, 0, 19, 7, 30)
+                val endTime: Calendar = Calendar.getInstance()
+                endTime.set(2012, 0, 19, 10, 30)
+
+                // We see these parameters from the intent's documentation.
+                calendarIntent.putExtra(
+                    CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                    beginTime.timeInMillis
+                )
+                calendarIntent.putExtra(
+                    CalendarContract.EXTRA_EVENT_END_TIME,
+                    endTime.timeInMillis
+                )
+                calendarIntent.putExtra(CalendarContract.Events.TITLE, "Ninja class")
+                calendarIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Secret dojo")
+                startActivity(calendarIntent)
+            }
+        }
+
+        // IMPLICIT INTENT - Opening a Web Site
+        btnGoToImplicitActivityWebSite.setOnClickListener {
+            val webpage: Uri = Uri.parse("https://www.android.com/")
+            val webIntent = Intent(Intent.ACTION_VIEW, webpage)
+            startActivity(webIntent)
         }
 
         editTextAge.addTextChangedListener { newText ->
